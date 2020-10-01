f1745514fb10bff7b13e6678779b42fd0bc3013e
daniellyncon <daniellyncon@hotmail.com>
2020-10-01 16:47:24 -0300

    days of week change

--- on_duty/models.py
+++ on_duty/models.py
@@ -5,7 +5,9 @@ from django.db import models
 
 
 class OnDuty(models.Model):
+    DAYS = (('1', 'Segunda-feira'), ('2', 'Terça-feira'), ('3', 'Quarta-feira'), ('4', 'Quinta-feira'),
+                ('5', 'Sexta-feira'))
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="duties", default=None)
-    day_of_the_week = models.IntegerField(verbose_name="Dia da semana")
+    day_of_the_week = models.CharField(max_length=10, choices=DAYS, blank=True, null=True, verbose_name='Dia da semana')
     start_time = models.TimeField(verbose_name="Início plantão")
     end_time = models.TimeField(verbose_name="Fim plantão")
