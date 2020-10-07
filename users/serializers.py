6b43225ad50f30f53dbb9406279fcaad44c312f3
daniellyncon <daniellyncon@hotmail.com>
2020-10-06 23:56:29 -0300

    writable relationship added to users

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,14 @@
-from rest_framework import serializers
+# from rest_framework import serializers
+from drf_writable_nested.serializers import WritableNestedModelSerializer
+from address.serializers import AddressSerializer
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    
+class UserSerializer(WritableNestedModelSerializer):
+    address = AddressSerializer()
+
     class Meta:
         model = User
-        fields = ('name', 'email', 'bond_type', 'phone', 'registration',
+        fields = ('name', 'email', 'bond_type', 'phone', 'registration', 'address',
                   'course', 'university', 'department', 'rg', 'cpf', 'cnh',
                   'date_joined', 'date_fired', 'is_active', 'scholarship', 'scholarship_type')
-
