41ea5d58df2d2edc858cb21b5e9b1818b069b2f5
daniellyncon <daniellyncon@hotmail.com>
2020-10-07 01:01:08 -0300

    writable nested relationship

--- people/serializers.py
+++ people/serializers.py
@@ -1,8 +1,10 @@
-from rest_framework import serializers
+from drf_writable_nested.serializers import WritableNestedModelSerializer
 from .models import Person
+from address.serializers import AddressSerializer
 
 
-class PersonSerializer(serializers.ModelSerializer):
+class PersonSerializer(WritableNestedModelSerializer):
+    address = AddressSerializer()
 
     class Meta:
         model = Person
--- users/serializers.py
+++ users/serializers.py
@@ -1,14 +1,15 @@
-# from rest_framework import serializers
 from drf_writable_nested.serializers import WritableNestedModelSerializer
 from address.serializers import AddressSerializer
+from on_duty.serializers import OnDutySerializer
 from .models import User
 
 
 class UserSerializer(WritableNestedModelSerializer):
     address = AddressSerializer()
+    on_duty = OnDutySerializer(required=False, allow_null=True, many=True)
 
     class Meta:
         model = User
         fields = ('name', 'email', 'bond_type', 'phone', 'registration', 'address',
-                  'course', 'university', 'department', 'rg', 'cpf', 'cnh',
+                  'course', 'university', 'department', 'rg', 'cpf', 'cnh', 'on_duty',
                   'date_joined', 'date_fired', 'is_active', 'scholarship', 'scholarship_type')
