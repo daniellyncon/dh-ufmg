799a2f2d0656613c2d36edd8c0bdfc73222593f3
Daniel Lyncon <43709270+daniellyncon@users.noreply.github.com>
2020-10-15 01:10:44 -0300

    alter cases

--- cases/serializers.py
+++ cases/serializers.py
@@ -3,15 +3,7 @@ from rest_framework import serializers
 
 
 class CaseSerializer(serializers.ModelSerializer):
-    # assisted_person
-    # advisor
-    # intern
-    # axis
-    # entities
-    # law_suits
-    # tasks
-    # documents
+    
     class Meta:
         model = Case
-        fields = ['case_number', 'related_areas',  'reference_contacts', 'daj_number',  'daj_advisor',  'daj_intern',
-        'report', 'registration_date', 'solution_date']
+        fields = '__all__'
