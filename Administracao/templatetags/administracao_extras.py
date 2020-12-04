378854f745bffd03a91b4d914bb2fc0230c43b82
Gabriel Chaves <gabrielmoreirachaves@outlook.com>
2020-12-04 17:26:13 -0300

    refactor: improve the random phrase method

--- Administracao/templatetags/administracao_extras.py
+++ Administracao/templatetags/administracao_extras.py
@@ -1,9 +1,10 @@
 from django import template
-
 from ..models import Frase
+from random import choice
 
 register = template.Library()
 
 @register.simple_tag
 def random_phrase():
-    return Frase.objects.order_by('?').first()
+    phrases = Frase.objects.all()
+    return choice(phrases) if phrases else None
